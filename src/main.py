a691697c7fa3299cfbd5ce293a05898d007ce85e
themaxpearce <max.pearce47@gmail.com>
2025-07-14 22:38:12 +0100

    Added Sync command and require bot mention

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ intents.message_content = True
 intents.messages = True
 intents.guilds = True
 intents.members = True
+#intents.mentions = True
 
 client = Gippity(command_prefix="g!", intents=intents)
 responder = Responder(ai_key)
@@ -23,9 +24,20 @@ responder = Responder(ai_key)
 async def on_ready():
     print("Bot Ready")
 
+@client.tree.command(name="sync", description="Owner only command")
+async def sync(interaction: discord.Interaction):
+    if interaction.user.id == 274620118795812864:
+        print("Requested tree sync")
+        await client.tree.sync()
+
+        print("Tree has synced")
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
+        return
+    
+    if client.user not in message.mentions:
         return
     
     instructions = await client.genInstructionsFromMessage(message)
